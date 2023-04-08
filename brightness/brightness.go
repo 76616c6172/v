31f6e7e7603d6b6d3a1c9b7c81cfc114812f900f
valar <68338647+76616c6172@users.noreply.github.com>
2023-04-08 16:27:47 -0700

    Initial commit

---
+++ brightness/brightness.go
@@ -0,0 +1,51 @@
+package brightness
+
+import (
+	"fmt"
+	"os/exec"
+
+	Z "github.com/rwxrob/bonzai/z"
+	"github.com/rwxrob/help"
+)
+
+var Cmd = &Z.Cmd{
+	Name:     `brightness`,
+	Summary:  `change screen brightness`,
+	Commands: []*Z.Cmd{help.Cmd, dimCmd, fullCmd},
+}
+
+var dimCmd = &Z.Cmd{
+	Name:    `dimmed`,
+	Summary: `80% screen brightness`,
+	Call: func(_ *Z.Cmd, args ...string) error {
+		level := "0.8"
+		cmd := exec.Command("xrandr", "--output", "eDP-1", "--brightness", level)
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("Error running xrandr command:", err)
+			return err
+
+		}
+		fmt.Println("screen brightness set to:", level)
+
+		return nil
+	},
+}
+
+var fullCmd = &Z.Cmd{
+	Name:    `full`,
+	Summary: `100% screen brightness`,
+	Call: func(_ *Z.Cmd, args ...string) error {
+		level := "1"
+		cmd := exec.Command("xrandr", "--output", "eDP-1", "--brightness", level)
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("Error running xrandr command:", err)
+			return err
+
+		}
+		fmt.Println("screen brightness set to:", level)
+
+		return nil
+	},
+}
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"log"
+
+	"github.com/76616c6172/v/brightness"
+	Z "github.com/rwxrob/bonzai/z"
+	"github.com/rwxrob/help"
+)
+
+func init() {
+	Z.Dynamic["uname"] = func() string { return Z.Out("uname", "-a") }
+	Z.Dynamic["ls"] = func() string { return Z.Out("ls", "-l", "-h") }
+}
+
+func main() {
+
+	// remove log prefixes
+	log.SetFlags(0)
+
+	// provide panic trace
+	Z.AllowPanic = true
+
+	// can run in multicall, or monolith, not both
+
+	/*
+		// MULTICALL (status, afk, etc. linked)
+		// (no completion unless set for individual commands)
+		// (requires creation of hard/sym links or copies)
+		Z.Commands = map[string][]any{
+			// "conf": {conf.Cmd}, // bork cuz no multicall mode
+			"yq":     {yq.Cmd},
+			"y2j":    {y2j.Cmd},
+			"status": {tmux, "update"},
+			"afk":    {twitch.Cmd, "chat", "!afk"},
+		}
+		Z.Run()
+	*/
+
+	Cmd.Run()
+}
+
+var Cmd = &Z.Cmd{
+
+	Name:      `v`,
+	Summary:   `CLI for controlling vulpix`,
+	Copyright: `Copyright 2023 valar`,
+	Version:   `v0.0.1`,
+	License:   `Apache-2.0`,
+	Site:      `github.com/76616c6172`,
+	Source:    `git@github.com:76616c6172/v.git`,
+	Issues:    `github.com/76616c6172/v/issues`,
+
+	Commands: []*Z.Cmd{
+		help.Cmd,
+		brightness.Cmd,
+
+		// openapi.Cmd, update.Cmd, goutil.Cmd
+	},
+
+	Shortcuts: Z.ArgMap{
+		//`project`:   {`twitch`, `bot`, `commands`, `edit`, `project`},
+		//`status`:    {`tmux`, `update`},
+		//`offscreen`: {`chat`, `!offscreen`},
+		//`info`:      {`twitch`, `bot`, `commands`, `file`, `edit`},
+		//`sync`:      {`twitch`, `bot`, `commands`, `sync`},
+		//`work`:      {`go`, `work`},
+		//`chat`:      {`twitch`, `chat`},
+		//`afk`:       {`twitch`, `chat`, `!afk`},
+		//`isosec`:    {`uniq`, `isosec`},
+		//`isonan`:    {`uniq`, `isonan`},
+		//`isodate`:   {`uniq`, `isodate`},
+		//`uuid`:      {`uniq`, `uuid`},
+		//`epoch`:     {`uniq`, `second`},
+		//`path`:      {`env`, `get`, `path`},
+		//`ytlink`:    {`filter`, `youtube`, `linkify`},
+		//`long version of path`: {`env`, `get`, `path`},
+	},
+
+	Description: `
+		o.o
+		`,
+}
